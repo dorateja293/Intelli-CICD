a27aac54082e89c8736dc866797c3bffb47767f0
dorateja293 <dorateja293@users.noreply.github.com>
2026-03-04 15:57:59 +0530

    Live API_URL added

--- github_ci_runner.py
+++ github_ci_runner.py
@@ -20,7 +20,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 
-API_URL = "http://127.0.0.1:5000/predict"
+API_URL = "https://intelli-cicd.onrender.com/predict"
 
 
 def fail(message):
